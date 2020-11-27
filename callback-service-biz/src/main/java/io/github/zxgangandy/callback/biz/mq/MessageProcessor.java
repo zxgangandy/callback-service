8e848a6eca9fff1ecb3a46ce1ac889856a3e1c58
andy zhao <286505491@qq.com>
2020-11-27 11:08:33 +0800

    refactor 消息处理;

--- callback-service-biz/src/main/java/io/github/zxgangandy/callback/biz/mq/MessageProcessor.java
+++ callback-service-biz/src/main/java/io/github/zxgangandy/callback/biz/mq/MessageProcessor.java
@@ -1,24 +1,15 @@
 package io.github.zxgangandy.callback.biz.mq;
 
 import io.andy.rocketmq.wrapper.core.consumer.processor.ConcurrentlyProcessor;
-import io.github.zxgangandy.callback.biz.bo.AddTaskReqBO;
 import io.github.zxgangandy.callback.biz.bo.AddTaskReqWrapperBO;
-import io.github.zxgangandy.callback.biz.entity.CallbackLog;
-import io.github.zxgangandy.callback.biz.entity.CallbackTask;
-import io.github.zxgangandy.callback.biz.service.ICallbackLogService;
 import io.github.zxgangandy.callback.biz.service.ICallbackTaskService;
-import io.jingwei.base.utils.net.HttpClientUtil;
-import io.jingwei.base.utils.tx.TxTemplateService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-import static io.github.zxgangandy.callback.biz.constant.CallSuccessStatus.FAILED;
-import static io.github.zxgangandy.callback.biz.constant.CallSuccessStatus.SUCCESS;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 @Component
@@ -28,11 +19,7 @@ public class MessageProcessor implements ConcurrentlyProcessor {
     @Autowired
     private ICallbackTaskService callbackTaskService;
 
-    @Autowired
-    private ICallbackLogService  callbackLogService;
 
-    @Autowired
-    private TxTemplateService    txTemplateService;
 
     @Override
     public ConsumeConcurrentlyStatus process(Object messageBody) {
@@ -41,7 +28,7 @@ public class MessageProcessor implements ConcurrentlyProcessor {
             processMessage(wrapper);
         } catch (Exception ex) {
             log.error("processMessage failed, ex={}", ex);
-            updateFailedResult(wrapper, EMPTY);
+            callbackTaskService.execFailedResult(wrapper, EMPTY);
             return ConsumeConcurrentlyStatus.RECONSUME_LATER;
         }
 
@@ -49,65 +36,9 @@ public class MessageProcessor implements ConcurrentlyProcessor {
     }
 
     private void processMessage(AddTaskReqWrapperBO reqBO) throws IOException {
-        final AddTaskReqBO req = reqBO.getReqBO();
-        final String targetUrl = req.getTargetUrl();
-        final String reqParam  = req.getReqParam();
-
-        String resp;
-        if (StringUtils.equalsIgnoreCase(req.getReqMethod(), "POST")) {
-            resp = HttpClientUtil.getInstance().postSync(targetUrl, reqParam);
-        } else {
-            resp = HttpClientUtil.getInstance().getSync(targetUrl, null);
-        }
-
-        String callSuccess = FAILED.getStatus();
-        if (req.getCallExpect().contains(resp)) {
-            callSuccess = SUCCESS.getStatus();
-        }
-
-        updateSuccessResult(reqBO, resp, callSuccess);
+        callbackTaskService.execTask(reqBO);
     }
 
-    private void updateSuccessResult(AddTaskReqWrapperBO wrapper, String callResult, String callSuccess) {
-        txTemplateService.doInTransaction(() -> {
-            updateCallResult(callSuccess, callResult, Long.parseLong(wrapper.getTaskId()));
-            saveLog(wrapper, callResult);
-        });
-    }
 
-    private void updateFailedResult(AddTaskReqWrapperBO wrapper, String callResult) {
-        txTemplateService.doInTransaction(() -> {
-            updateCallResult(FAILED.getStatus(), callResult, Long.parseLong(wrapper.getTaskId()));
-            saveLog(wrapper, callResult);
-        });
-    }
-
-    private void updateCallResult(String callSuccess, String resp, long taskId) {
-        callbackTaskService.lambdaUpdate()
-                .set(CallbackTask::getCallSuccess, callSuccess)
-                .set(CallbackTask::getCallResult, resp)
-                .setSql("call_count = call_count + 1")
-                .eq(CallbackTask::getTaskId, taskId)
-                .update();
-    }
-
-    private void saveLog(AddTaskReqWrapperBO wrapper, String callResult) {
-        callbackLogService.save(createCallbackLog(wrapper, callResult));
-    }
-
-    private CallbackLog createCallbackLog(AddTaskReqWrapperBO wrapper, String callResult) {
-        final AddTaskReqBO req = wrapper.getReqBO();
-        final long taskId = Long.parseLong(wrapper.getTaskId());
-
-        return new CallbackLog().setTaskId(taskId)
-                .setBizType(req.getBizType())
-                .setReqMethod(req.getReqMethod())
-                .setReqParam(req.getReqParam())
-                .setSourceApp(req.getSourceApp())
-                .setSourceIp(req.getSourceIp())
-                .setTargetApp(req.getTargetApp())
-                .setTargetUrl(req.getTargetUrl())
-                .setCallResult(callResult);
-    }
 
 }
