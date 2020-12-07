1c660434a333817004c3c51355590163c003afe5
andy zhao <286505491@qq.com>
2020-12-07 11:11:15 +0800

    完善注册接口;

--- callback-service-biz/src/main/java/io/github/zxgangandy/callback/biz/exception/CallbackErrCode.java
+++ callback-service-biz/src/main/java/io/github/zxgangandy/callback/biz/exception/CallbackErrCode.java
@@ -7,16 +7,7 @@ public enum CallbackErrCode implements IBizErrCode {
     TASK_NOT_FOUND("10000", "task-not-found"),
     SEND_MSG_FAILED("10001", "send-message-failed"),
     CALL_BACK_FAILED("10002", "call-third-party-failed"),
-    MSG_TOPIC_NOT_REG("10003", "message-topic-not-register"),
-    ILLEGAL_MERCHANT_ID("12304", "illegal-merchant-id"),
-    MERCHANT_DISABLED("12305", "merchant-disabled"),
-    MERCHANT_NOT_INIT("12306", "merchant-not-init"),
-    SECRET_KEY_EXPIRED("12307", "secret-key-expired"),
-    ORDER_NOT_FOUND("12308", "order-not-found"),
-    TRIGGER_PRICE_INVALID("12008", "trigger-price-invalid"),
-    ORDER_VOLUME_INVALID("12009", "order-volume-invalid"),
-    ORDER_TYPE_NOT_SUPPORT("12010", "order-type-not-support"),
-    PLACE_ORDER_FAILED("12015", "place-order-failed")
+    MSG_TOPIC_NOT_REG("10003", "message-topic-not-register")
     ;
 
     /**
