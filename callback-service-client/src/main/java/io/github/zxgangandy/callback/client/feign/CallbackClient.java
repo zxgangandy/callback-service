d4fcb04f8dd52bb71e76a451fa4556ffddaf4b8b
andy zhao <286505491@qq.com>
2020-12-23 18:49:50 +0800

    add feign client;

--- callback-service-client/src/main/java/io/github/zxgangandy/callback/client/feign/CallbackClient.java
+++ callback-service-client/src/main/java/io/github/zxgangandy/callback/client/feign/CallbackClient.java
@@ -4,14 +4,14 @@ import io.github.zxgangandy.callback.model.AddTaskReq;
 import io.github.zxgangandy.callback.model.AddTaskResp;
 import io.jingwei.base.utils.model.R;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
-@FeignClient("callback-service")
+@FeignClient(value = "callback-service")
 public interface CallbackClient {
     String baseUrl = "/api/callback/v1";
 
-    @GetMapping({baseUrl + "/add_task"})
+    @PostMapping({baseUrl + "/add_task"})
     R<AddTaskResp> addTask(AddTaskReq req);
 
 }
